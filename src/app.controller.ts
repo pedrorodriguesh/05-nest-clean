917601847ff8190475fdb6ed6118d06b1ce3794b
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-04-24 20:08:36 -0300

    create prisma service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 // NestJS is a framework that uses decorators to define the structure of the application.
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   // Identifies the route for the method
   @Get('/hello')
-  getHello(): string {
+  getHello() {
     return this.appService.getHello()
   }
 }
