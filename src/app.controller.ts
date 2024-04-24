f7a9abc839ad79e357129d87acf4f1f90a86f7f8
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-04-24 19:30:31 -0300

    eslint setup and removing some code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
+// NestJS is a framework that uses decorators to define the structure of the application.
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  // Identifies the route for the method
+  @Get('/hello')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 }
