c69fad59f67e34004e5e739ee8cf3c47f3b26185
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-04-25 21:21:01 -0300

    personalized decorator to create-question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
+import { CurrentUser } from 'src/auth/current-user.decorator'
+import { UserPayload } from 'src/auth/jwt.strategy'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('/questions')
@@ -8,7 +10,9 @@ export class CreateQuestionController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user.sub)
+
     return 'ok'
   }
 }
