d6c6e7be8b4edb1cd6c10b33478cccef36307e69
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-04-26 18:04:52 -0300

    tests e2e accounts

---
+++ src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { INestApplication } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Test, TestingModule } from '@nestjs/testing'
+import { hash } from 'bcryptjs'
+import request from 'supertest'
+import { AppModule } from '../app.module'
+import { PrismaService } from '../prisma/prisma.service'
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+      providers: [PrismaService, ConfigService],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    prisma = moduleRef.get(PrismaService)
+
+    await app.init()
+  })
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: await hash('123456', 8),
+      },
+    })
+
+    const auth = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(auth.statusCode).toBe(201)
+    expect(auth.body).toEqual({
+      acess_token: expect.any(String),
+    })
+  })
+})
