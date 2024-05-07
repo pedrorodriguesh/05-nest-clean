a6c8feaa724c60ca29bd85e7c1f6798e04ab3a41
Pedro Henrique Rodrigues <pedrorodrigueshdev@gmail.com>
2024-05-06 23:19:55 -0300

    comments about prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
 
+// Prisma service that extends the PrismaClient class and implements the OnModuleInit and OnModuleDestroy interfaces.
 @Injectable()
 export class PrismaService
   extends PrismaClient
